a98b4c8ffdce3dbbcce62465da256361b13e159a
vishalsinganjude <vsinganjude8@gmail.com>
2017-08-30 22:01:28 +0530

    Sample Student Constructor for StudentData.

---
+++ MutexPractice/src/collection/Student.java
@@ -0,0 +1,12 @@
+package collection;
+
+public class Student
+{
+    String name;
+    int rollno;
+    Student(String Name,int Rollno)
+    {
+        this.name = Name;
+        this.rollno =Rollno;
+    }
+}
